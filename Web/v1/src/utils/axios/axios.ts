8ed844e9a50a4326de9459d14bfa877713323caf
L1nSn0w <l1nsn0w@qq.com>
2024-08-25 21:18:40 +0800

    chore: import some env and packaging two axios instance

---
+++ Web/v1/src/utils/axios/axios.ts
@@ -0,0 +1,54 @@
+// packaging the axios instance
+import axios from 'axios'
+
+// Create an axios instance
+
+// For Auth Request
+const authClient = axios.create(
+    {
+        baseURL: import.meta.env.VITE_AUTH_URL, // get the auth url from .env file
+        timeout: 10000, // set the timeout to 10s
+        headers: {
+            'Content-Type': 'application/json', // set the content type to json
+        }
+    }
+)
+
+// For API Request
+const apiClient = axios.create({
+    baseURL: import.meta.env.VITE_API_URL, // get the api url from .env file
+    timeout: 10000, // set the timeout to 10s
+})
+
+// Add a interceptor to the axios instance
+apiClient.interceptors.request.use(
+    // Dynamically set the Content-Type header
+    (config) => {
+        if (config.data) {
+            if (config.data instanceof FormData) {
+                config.headers['Content-Type'] = 'multipart/form-data'
+            } else if (typeof config.data === 'string') {
+                config.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+            } else {
+                config.headers['Content-Type'] = 'application/json'
+            }
+        }
+        
+        return config
+    },
+    (error) => Promise.reject(error)
+)
+
+// Response interceptor
+apiClient.interceptors.response.use(
+    (Response) => Response,
+    (error) => {
+        // handle the error
+        console.log('Response Error:', error)
+        return Promise.reject(error)
+    }
+)
+
+
+// Export the axios instance
+export { authClient, apiClient}
--- Web/v1/vite.config.ts
+++ Web/v1/vite.config.ts
@@ -29,5 +29,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+
+  // server: {
+  //   proxy: {
+
+  //   },
+  // },
 })
